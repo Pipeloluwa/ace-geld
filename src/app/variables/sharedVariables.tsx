15beb51fe02f178ca7e18e2bbbc306736bb09792
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-12-01 18:52:01 +0100

    added

--- src/app/variables/sharedVariables.tsx
+++ src/app/variables/sharedVariables.tsx
@@ -10,7 +10,7 @@ import { MdCable, MdHub } from 'react-icons/md';
 
 
 
-export const rootUrl = "https://ace-geld-git-main-papic-nation.vercel.app/";
+export const rootUrl = "https://ace-geld.vercel.app/";
 
 
 
