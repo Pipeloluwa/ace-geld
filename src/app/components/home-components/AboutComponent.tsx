3c89ce15ba39976982d1367a6498a3c51b215560
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-12-01 18:31:29 +0100

    Update AboutComponent.tsx

--- src/app/components/home-components/AboutComponent.tsx
+++ src/app/components/home-components/AboutComponent.tsx
@@ -22,20 +22,6 @@ import {
 
 
 export const AboutComponent = () => {
-    const countUpRef = React.useRef(null);
-    const { start, pauseResume, reset, update } = useCountUp({
-        ref: countUpRef,
-        start: 0,
-        end: 1234567,
-        delay: 1000,
-        duration: 5,
-        onReset: () => console.log('Resetted!'),
-        onUpdate: () => console.log('Updated!'),
-        onPauseResume: () => console.log('Paused or resumed!'),
-        onStart: ({ pauseResume }) => console.log(pauseResume),
-        onEnd: ({ pauseResume }) => console.log(pauseResume),
-    });
-
 
     const divCountRef: LegacyRef<HTMLDivElement> = useRef(null);
     const divCountView = useInView(divCountRef);
